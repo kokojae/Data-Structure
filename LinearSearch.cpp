c9a6ea781f7d441d70fd40062bf7c4fae26ebbdb
Ko Min Jae <54296776+kokojae@users.noreply.github.com>
2021-02-01 10:37:21 +0900

    Complete LinearSearch.cpp
    LinearSearch.cpp 완성함

--- LinearSearch.cpp
+++ LinearSearch.cpp
@@ -15,7 +15,17 @@ int main()
 	int arr[] = { 3,5,2,4,9 };
 	int idx;
 
+	idx = LSearch(arr, sizeof(arr) / sizeof(int), 4);
+	if (idx == -1)
+		printf("Å½»ö ½ÇÆĞ\n");
+	else
+		printf("Å¸°Ù ÀúÀå ÀÎµ¦½º: %d\n", idx);
 
+	idx = LSearch(arr, sizeof(arr) / sizeof(int), 7);
+	if (idx == -1)
+		printf("Å½»ö ½ÇÆĞ\n");
+	else
+		printf("Å¸°Ù ÀúÀå ÀÎµ¦½º: %d\n", idx);
 
 	return 0;
 }
